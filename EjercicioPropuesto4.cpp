20e4ab04b48f4e14ef84d02a8706424c83c20e42
Rodrigo Bernardo Condori Gutierrez <94139064+RodrigoCG1281@users.noreply.github.com>
2022-11-17 23:29:02 -0500

    Add files via upload

---
+++ EjercicioPropuesto4.cpp
@@ -0,0 +1,16 @@
+/*4.- Escriba un programa que calcule el área de un cuadrado cuyos lados miden 5m*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int lado_cuadrado;
+
+    cout<<"Ingrese el valor de un lado del cuadrado: "<<endl;
+    cin>>lado_cuadrado;
+
+    cout<<"El area del cuadrado es: "<<lado_cuadrado * lado_cuadrado;
+
+    return 0;
+}

1392102a9fa15cd2ad4e5fb2b5b055868ad4e061
Rodrigo Bernardo Condori Gutierrez <94139064+RodrigoCG1281@users.noreply.github.com>
2022-11-17 21:54:25 -0500

    Iniciando el curso

---
+++ Tipos de variable.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    //Aqui podemos usar short, unsigned and long
+
+    short int numero = 10;
+    long int numero2 = 3000;
+    unsigned int numero3 = 234234;
+    float numero4 = 15.346;
+    double numero5 = 234234234.234234234;
+
+    char a = 'R';
+
+    cout<<numero<<endl;
+    cout<<numero2<<endl;
+    cout<<numero3<<endl;
+    cout<<numero4<<endl;
+    cout<<numero5<<endl;
+    cout<<a<<endl;
+
+
+    return 0;
+}

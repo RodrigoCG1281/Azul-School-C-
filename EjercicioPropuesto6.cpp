89f549e2a842fcd63ea18d113af98212ad9403f6
Rodrigo Bernardo Condori Gutierrez <94139064+RodrigoCG1281@users.noreply.github.com>
2022-11-17 23:54:22 -0500

    Add files via upload

---
+++ EjercicioPropuesto6.cpp
@@ -0,0 +1,25 @@
+/*6.- Crea un programa que imprima el resultado del siguiente problema: 
+Dos personas ganan entre los 2 un total de $3,000. 
+¿Si una gana $500 más que otra, cuánto gana cada una? 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+
+    a = 2500 / 2;
+    b = a + 500;
+
+    cout<<a<<endl;
+    cout<<b;
+
+    
+ 
+
+
+
+
+    return 0;
+}
